70043524872972e5cb1ba7ddae1111b79a04a7e5
Dmytro Kudashev <kudashev@ppg.com>
2020-04-08 17:13:23 +0200

    Dodałem przylkad jak wydoścać wartośc ze środku funkcji, oraz opis do tego

--- wczy.js
+++ wczy.js
@@ -8,7 +8,21 @@
         
         const previewDefaultText = previewContainer.querySelector(".image-preview__default-text");
         
-        
+        //Prede wszystkim macie zmianą inFile, ale event change jest podpinany pod inpFile, kt~ora nie jest zadeklarowana. Moim zdaniem musicie miec event podpięty pod inFile.
+        //wyslałem wam linka odnośnie scope w javascript, w tym przypadku finkcja addEventListener jest osobnym skopem, scope gdzie jesr zadeklarowanna zmianna inFile jest tak zwany global scope
+        //i tu możecoe dodać (zadeklarować) jeszcze jedną zmianna a (przeż "let a") i przepisać do niej wartość wewnątrz funkcji.
+        // no i eventualnie użyć jej pózniej, ale prosze pamentać, że wartość (ta co w środku funkcji) może byv nie przypisana jeżeli event nie nastąmpi, i zmiana będie miala wartosc undefined
+        //dlatego musicie sprawdzić czy zmiana ma przepisaną wartość
+        //let a;
+        //inFile.addEventListener("change", function(){
+        // a = "jakas wartość"; 
+        //}
+        //...
+        // if (a) { // jeżeli a ma wartość (ty wchodzą w gre tak zwane trusness & falseness), chyli jest sprawdzane czy a ma jakaś wartość odmianą od false, 0, null, undefined
+        //     //a ma wartość robcie co jest potrzeble z tą wartościa
+        //     console.log(a);
+        // }
+
         inpFile.addEventListener("change", function(){ 
         
             const file = this.files[0];

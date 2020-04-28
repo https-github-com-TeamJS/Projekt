7ea55e2eae116e83e550731949bd9d6051a65cc3
Dmytro Kudashev <kudashev@ppg.com>
2020-04-28 20:30:25 +0200

    Dodałem komentarze do kodu, oraz pokazałem jak jeszcze się da wyświetlać kolory na rózny sposób

--- aktualne/wczy.js
+++ aktualne/wczy.js
@@ -1,46 +1,37 @@
-      
-    
-       const inFile = document.getElementById("inpFile");
-       
-       const previewContainer = document.getElementById("imagePreview");
-       
-       const previewImage = previewContainer.querySelector(".image-preview__image");
-       
-       const previewDefaultText = previewContainer.querySelector(".image-preview__default-text");
+const inFile = document.getElementById("inpFile");
+//moim zdaniem dwie następne linije są zbędne
+const previewContainer = document.getElementById("imagePreview");
+const previewImage = previewContainer.querySelector(".image-preview__image");
 
-       
-    
-       
+// elementa image-preview__default-text chyba nie jest na widoku, i nie widzę że coś z nim robione, chyba jest zbędny
+const previewDefaultText = previewContainer.querySelector(".image-preview__default-text");
 
+inpFile.addEventListener("change", function () {  //addEventListener metoda rejestruje "obserwatora" do okreslnego typu zdarzenia
+    const file = this.files[0];
 
-       inpFile.addEventListener("change", function(){  //addEventListener metoda rejestruje "obserwatora" do okreslnego typu zdarzenia
-                                                      
-           const file = this.files[0];
-           
-           if (file) {
-               const reader = new FileReader();
-               
-               reader.addEventListener("load", function() {
-               console.log(this); 
-               
-               previewImage.setAttribute("src", this.result); //setAttribute dodaje nowy lub zmienia wartość atrybutu w bieżącym elemencie.
+    if (file) {
+        const reader = new FileReader();
 
-                
-               previewImage.addEventListener("load", color_picker)
+        reader.addEventListener("load", function () {
+            // console.log ma być usunienty przed tym ja wbijacie kod
+            console.log(this);
             
-            
-           });
-           
-               
-               reader.readAsDataURL(file);
-           }
+            // moim zdaniem nie potrzbnie używacie prewiew image. Wyglada na to że jest używane tylko po to żeby przekazać base64 do funkcji żęby pózniej wypelnić canvas. 
+            //To dla czego nie przekazać base64 orazy do funkcji.
+            //color_picker(this.result);
 
-           
-           else {
-               previewDefaultText.style.display = null;
-               previewImage.style.display = null;
-               previewImage.setAttribute("src", "");
-           }
-        
-       });
+            previewImage.setAttribute("src", this.result); //setAttribute dodaje nowy lub zmienia wartość atrybutu w bieżącym elemencie.
+            previewImage.addEventListener("load", color_picker); // srednik zapomnialiście
+        });
+
+        reader.readAsDataURL(file);
+     } 
+     // to też nie jest potrzebne, caly else, chyba że chcecie pokażać na początku użytkownikowi żeby wybrał plik, ale jak pliku nie wybrał to nić się nie musi dzałać
+     // oczywiszcze jak usuniecie previewImage którego tak naprawdę nie potrzebujecie
+     else {
+        previewDefaultText.style.display = null; // ta linija też jest zbędna
+        previewImage.style.display = null;
+        previewImage.setAttribute("src", "");
+    }
+});
 
--- aktualne/wczy2.js
+++ aktualne/wczy2.js
@@ -1,66 +1,104 @@
-function color_picker(){ // this jest naszym obrazkiem
-             
-              
-  const base64 = this.getAttribute("src");
-  
-     
-   var canvas = document.getElementById("canvas");
+function color_picker(base64) { // this jest naszym obrazkiem
+    //przekazywane z gory zabrałem konstante
+    base64 = this.getAttribute("src");
 
-   function getElementPosition(obj) {
-       var curleft = 0, curtop = 0;
-       if (obj.offsetParent) {
-           do {
-               curleft += obj.offsetLeft;
-               curtop += obj.offsetTop;
-           } while (obj = obj.offsetParent);
-           return { x: curleft, y: curtop };
-       }
-       return undefined;
-   }
-   
-   function getEventLocation(element,event){
-           var pos = getElementPosition(element);
-       
-       return {
-               x: (event.pageX - pos.x),
-             y: (event.pageY - pos.y)
-       };
-   }
-   //funcja zamieniajaca rgb na hex
-   function rgbToHex(r, g, b) {
-       if (r > 255 || g > 255 || b > 255)
-           throw "Invalid color component";
-       return ((r << 16) | (g << 8) | b).toString(16);
-   }
-   
-   function drawImageFromWebUrl(sourceurl){
-         var img = new Image();
-   
-         img.addEventListener("load", function () {
-             // The image can be drawn from any source
-             canvas.getContext("2d").drawImage(img, 0, 0, img.width,    img.height, 0, 0, canvas.width, canvas.height);
-             
-         });
-   
-         img.setAttribute("src", sourceurl);
-   }
-  
-   drawImageFromWebUrl(base64);
-   
-   canvas.addEventListener("mousedown",function(e){
-      var eventLocation = getEventLocation(this,e);
-       // Get the data of the pixel according to the location generate by the getEventLocation function
-       var context = this.getContext('2d');
-       var pixelData = context.getImageData(eventLocation.x, eventLocation.y, 1, 1).data; 
+    // canvas może być const, nie zmieniacie ten element tylko jego propy  
+    const canvas = document.getElementById("canvas");
 
-       var hex = "#" + ("000000" + rgbToHex(pixelData[0], pixelData[1], pixelData[2])).slice(-6);
-       
-       // Draw the color and coordinates.
-       document.getElementById("color").style.backgroundColor = hex;
-       document.getElementById("status").innerHTML = hex;
+    function getElementPosition(obj) {
+        var curleft = 0, curtop = 0;
+        if (obj.offsetParent) {
+            do {
+                curleft += obj.offsetLeft;
+                curtop += obj.offsetTop;
+            } while (obj = obj.offsetParent);
+            return { x: curleft, y: curtop };
+        }
+        // w tym przypadku, i akurat w tym warto się zastanowić czy nie zwrócic dom~yslne wartosci, bo pózniej wam wywali się błąd
+        return undefined;
+    }
 
-      
-      
-   },false);
-  
+    function getEventLocation(element, event) {
+        var pos = getElementPosition(element);
+        // tu będzie błąd z undefined wartosciami o ktorym piśalem wam wyżej
+        return {
+            x: (event.pageX - pos.x),
+            y: (event.pageY - pos.y)
+        };
+    }
+
+    //funcja zamieniajaca rgb na hex
+    function rgbToHex(r, g, b) {
+        if (r > 255 || g > 255 || b > 255)
+            throw "Invalid color component";
+        return ((r << 16) | (g << 8) | b).toString(16);
+    }
+
+    function drawImageFromWebUrl(sourceurl) {
+        var img = new Image();
+
+        img.addEventListener("load", function () {
+            // The image can be drawn from any source
+            canvas.getContext("2d").drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
+
+        });
+
+        img.setAttribute("src", sourceurl);
+    }
+
+    drawImageFromWebUrl(base64);
+
+    canvas.addEventListener("mousedown", function (e) {
+        //wszystkie zmianne mogą być konstantami
+        const eventLocation = getEventLocation(this, e);
+        // Get the data of the pixel according to the location generate by the getEventLocation function
+        const context = this.getContext('2d');
+        const pixelData = context.getImageData(eventLocation.x, eventLocation.y, 1, 1).data;
+
+        const hex = "#" + ("000000" + rgbToHex(pixelData[0], pixelData[1], pixelData[2])).slice(-6);
+        
+        // Draw the color and coordinates.
+        document.getElementById("color").style.backgroundColor = hex; 
+        document.getElementById("status").innerHTML = hex;
+        
+        //zrobiłem tu funkcje żeby wam pokazać jak jeszcze się da wyswietlić kolory
+        displayColors(pixelData);
+    });
+
+    function displayColors(pixelData) {
+        //rownież tak można ten color wyswietlić 
+        const rgb = "rgb(" + pixelData[0] + "," + pixelData[1] + "," + pixelData[2] + ")";
+        document.getElementById("rgb-color").style.backgroundColor = rgb;
+        document.getElementById("rgb-status").innerHTML = rgb;
+
+        //ogoólnie wrzucanie elementów do DOMa nie jest łatwe, ale dla przykladu to może być zrobione tak na pale. W tym lepiej używać jakichoś templatek czy frameworków.
+        // albo z odcieniami
+        const colorContainer = document.querySelector("#colorContainer");
+
+        //wyczyścić poprzedni kontent
+        colorContainer.innerHTML = ""
+
+        //tworze element do textu
+        const label = document.querySelector("#paleta");
+        //przypisuje text
+        label.innerText = "Paleta kolorów"
+        //dodaje do konteneru "autko"
+
+        //jasność będzi`e si`e zmieniała o 10%, ale da się ustalić mniejszy krok. im mniejszy będzie krok tym plyniej będzie się zmieniał kolor slupka.
+        const step = 0.1;
+        //i jest większe od step bo przy i = 0 kolor będzie biały, dla tego wyswietlam wszystko oprócz białego
+        for (let i = 1; i >= step; i = i - step) {
+            console.log(i);
+            const rgba = "rgba(" + pixelData[0] + "," + pixelData[1] + "," + pixelData[2] + "," + i +")";
+            // tworzę element div
+            const color = document.createElement("div");
+            // prypisuję klasę styli
+            color.className = "palete-color";
+            // nadałem kolor tla
+            color.style.backgroundColor = rgba;
+            // wkleilem jako dziecko elementa "autko"
+            colorContainer.appendChild(color);
+        }
+    }
+
 }
